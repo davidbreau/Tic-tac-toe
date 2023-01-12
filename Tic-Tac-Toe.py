b994392dd465317acce957da84d8dc824e74f661
davidbreau <davidbreau@live.fr>
2023-01-12 16:32:07 +0100

    ajout des commentaires de fonctions + ajout de la fonctionnalité si_egalite

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -6,10 +6,17 @@ grille = { "A1" : "  ", "A2" : "  ", "A3" : "  ",
 }
 
 def tictactoe():
-    print("### TICTACTOE ###")
+    """_summary_
+    Génére le message de lancement du jeu
+    """
+    print("### TIC TAC TOE ###")
     premier_joueur_aleatoire()
     
 def afficher_la_grille():
+    """_summary_
+    Affiche la grille avec contour fixe
+    Affiche le contenu des cases stockées dans la grille:dict
+    """
     print(
         "  ---- ---- ----", "\n", 
         "|", grille["A1"],"|",grille["B1"],"|",grille["C1"],"|", "1", "\n",
@@ -21,12 +28,11 @@ def afficher_la_grille():
         "  A    B     C","\n"
     )        
 
-def premier_joueur_aleatoire() -> str:
+def premier_joueur_aleatoire():
     """_summary_
     Détermine aléatoirement quel joueur doit commencer
-    Returns:
-        str: _description_
-    Renvoie un texte précisant quel joueur commence
+    Affiche le jouer determiner
+    Lance le premier tour de joeur
     """
     if random_int == 0:
         print("\n","LES ⭕ COMMENCENT !","\n")
@@ -39,34 +45,52 @@ def premier_joueur_aleatoire() -> str:
 
         
 def croix_joue():
-    ch_x = input("C'est aux ❌ de jouer.\nSur quelle case voulez-vous jouer ? : ")
-    if grille.get(ch_x) == None:
+    """_summary_
+    Process lorsque c'est aux croix de jouer
+    Attends un input valide des cases jouables
+    Previent le joueur si case invalide
+    """
+    choix_x = input("C'est aux ❌ de jouer.\nSur quelle case voulez-vous jouer ? : ").upper()
+    if grille.get(choix_x) == None:
         print("### SAISIE NON VALIDE ###")
         croix_joue()
-    elif grille.get(ch_x) != "  ":
+    elif grille.get(choix_x) != "  ":
         print("### CASE DEJA JOUÉE ###")
         croix_joue()
     else:    
-        grille[ch_x] = "❌"
+        grille[choix_x] = "❌"
         afficher_la_grille()
         si_victoire()
+        si_egalite()
         rond_joue()
         
 def rond_joue():
-    ch_r = input("C'est aux ⭕ de jouer.\nSur quelle case voulez-vous jouer ? : ")
-    if grille.get(ch_r) == None:
+    """_summary_
+    Process lorsque c'est aux ronds de jouer
+    Attends un input valide des cases jouables
+    Previent le joueur si case invalide
+    """
+    choix_o = input("C'est aux ⭕ de jouer.\nSur quelle case voulez-vous jouer ? : ").upper()
+    if grille.get(choix_o) == None:
         print("### SAISIE NON VALIDE ###")
         rond_joue()
-    elif grille.get(ch_r) != "  ":
+    elif grille.get(choix_o) != "  ":
         print("### CASE DEJA JOUÉE ###")
         rond_joue()
     else:    
-        grille[ch_r] = "⭕"
+        grille[choix_o] = "⭕"
         afficher_la_grille()
         si_victoire()
+        si_egalite()
         croix_joue()
+    
         
 def si_victoire():
+    """_summary_
+    Vérifie les scores
+    Arrête la partie en case de victoire
+    Affiche le gagnant
+    """
     row1 = [ grille["A1"], grille["A2"], grille["A3"]]
     row2 = [ grille["B1"], grille["B2"], grille["B3"]]
     row3 = [ grille["C1"], grille["C2"], grille["C3"]]
@@ -89,5 +113,18 @@ def si_victoire():
                   "### BRAVO ❌ VOUS AVEZ GAGNÉ !!! ###", "\n", 
                   "####################################")
             quit()
-            
+
+          
+def si_egalite():
+    """
+    Vérifie si le jeu est encore jouable
+    Arrête la partie si égalité
+    """
+    if grille["A1"] != "  " and grille["A2"] != "  " and grille["A3"] != "  " and grille["B1"] != "  " and grille["B2"] != "  " and grille["B3"] != "  " and grille["C1"] != "  " and grille["C2"] != "  " and grille["C3"] != "  ":
+        print(" ###################", "\n",
+            "### ÉGALITÉ !!! ###", "\n", 
+            "###################")
+        quit()
+    
+    
 tictactoe()
