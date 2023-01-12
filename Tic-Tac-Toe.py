3f64a5389261b2e9047a84ce3203f57ac7459781
davidbreau <davidbreau@live.fr>
2023-01-12 10:39:24 +0100

    ajout des données de la grille vide, ajout de la commmande pour afficher la grille dans le terminal

---
+++ Tic-Tac-Toe.py
@@ -0,0 +1,15 @@
+import random
+
+grille = [
+    { "A1" : " ", "A2" : " ", "A3" : " "},
+    { "B1" : " ", "B2" : " ", "B3" : " "},
+    { "C1" : " ", "C2" : " ", "C3" : "x"}
+]
+
+print(" --- --- ---")
+print("|", grille[0]["A1"],"|",grille[0]["A2"],"|",grille[0]["A3"],"|")
+print(" --- --- ---")
+print("|", grille[1]["B1"],"|",grille[1]["B2"],"|",grille[1]["B3"],"|")
+print(" --- --- ---")
+print("|", grille[2]["C1"],"|",grille[2]["C2"],"|",grille[2]["C3"],"|")
+print(" --- --- ---")
